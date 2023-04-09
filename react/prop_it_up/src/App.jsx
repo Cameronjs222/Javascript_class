9b2bbcae6e225971a337c17b70825190698573cf
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-04-08 17:09:55 -0700

    first jsx

---
+++ react/prop_it_up/src/App.jsx
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div id="container">
+        <h1>Hello Dojo</h1>
+        <p>Things I need to do</p>
+        <ul>
+          <li>Learn how to code in react.</li>
+          <li>Finish discussion questions.</li>
+          <li>do more algorithm problems.</li>
+          <li>Finish reading my book. </li>
+        </ul>
+        </div>
+      </header>
+    </div>  
+  );
+}
+
+export default App;
